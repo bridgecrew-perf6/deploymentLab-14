80cc14fb735c2ab947f4f55eafffc208b9c607be
Stetson Done <scdone@gmail.com>
2022-01-13 12:15:54 -0700

    added rollbar info

--- server/controller.js
+++ server/controller.js
@@ -1,7 +1,9 @@
+const Rollbar = require("rollbar")
 const hikers = require(`./db.json`)
 
 module.exports = {
     getHikers: (req,res) => {
         res.status(200).send(hikers)
+        Rollbar.info('hiker list was requested')
     }
 }
